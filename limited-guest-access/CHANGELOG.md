fd52dd53403eefc6dd3385633cfa85010bef9325
iesus <iesus@webbhuset.se>
2020-09-05 15:22:17 +0200

    added TLS support

--- limited-guest-access/CHANGELOG.md
+++ limited-guest-access/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.7] - 2020-09-05
+### Feature
+- Added TLS support
+
 ## [0.1.6] - 2020-09-05
 ### Bugfix
 - High CPU usage
--- limited-guest-access/run.sh
+++ limited-guest-access/run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/with-contenv bashio
 chmod 777 /data/
 
+is_ssl_active=$(cat /data/options.json |jq .activate_tls)
+if [ $is_ssl_active = true ]; then
+    if test -f "/ssl/privkey.pem"; then
+        sed  -i 's/  listen 8888 default_server;/  include \/etc\/nginx\/snippets\/tls.conf;/g' /etc/nginx/conf.d/default.conf
+    fi;
+fi;
+
 if ! pgrep "nginx" > /dev/null; then
     nginx && php-fpm7
 fi
