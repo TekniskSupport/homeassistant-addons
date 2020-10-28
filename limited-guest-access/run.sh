ed73134604698cd57fbe8eb21a407e8f2d578c45
iesus <iesus@webbhuset.se>
2020-10-28 08:23:39 +0100

    permission change to options.json

--- limited-guest-access/run.sh
+++ limited-guest-access/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 chmod 777 /data/
-
+chmod 644 /data/options.json
 is_ssl_active=$(cat /data/options.json |jq .activate_tls)
 if [ $is_ssl_active = true ]; then
     if test -f "/ssl/privkey.pem"; then
