f622a9ea74b49f0950686d31dfa756e2e5719750
chsln <lorenzo.chiesa@gmail.com>
2025-10-31 09:50:04 +0000

    modified:   limited-guest-access/app/user/index.php

--- limited-guest-access/app/user/index.php
+++ limited-guest-access/app/user/index.php
@@ -143,7 +143,7 @@ if ($actions->passwordProtected && !$actions->authenticated) :?>
         <?php endif; ?>
         <form action='?' method="post">
             <input name='password' type="password" placeholder="password">
-            <input type="submit" value="Login" />
+            <input type="submit" value="Login" class="login-button" />
         </form>
     </div>
 <?php
